ac5937153901d09322945945fb84421ddde0235e
mctdavid <36972430+mctdavid@users.noreply.github.com>
2019-09-29 19:25:28 -0700

    Update formatting.ps1

--- formatting.ps1
+++ formatting.ps1
@@ -1,1 +1,6 @@
-
+PS C:\Users\foxadmin> $data = get-service | where-object status -eq 'stopped' | select-object name,status
+PS C:\Users\foxadmin> $data
+PS C:\Users\foxadmin> $data | out-file .\services.csv
+PS C:\Users\foxadmin> notepad .\services.csv
+PS C:\Users\foxadmin> $data | export-csv .\services2.csv
+PS C:\Users\foxadmin> get-content .\services2.csv |more
