66f3cfcaa7043d525868339a97ea681ab684ffd9
mctdavid <36972430+mctdavid@users.noreply.github.com>
2019-09-30 19:53:25 -0700

    Update baby.ps1

--- baby.ps1
+++ baby.ps1
@@ -1,19 +1,20 @@
 #Command History
 get-history | out-file c:\history.txt
 
+#Capture Powershell Console Activity
 Start-transcript -path c:\transcript.txt -append
 stop-transcript
 
 get-service -computername Client01
 Enter-PSsession -computername Client01
 
+#Enable local server for PS remoting
 enable-psremoting -force
-Set-PSSessionConfiguration -name Microsoft.powershell -showsecuritydescriptorUI
+Set-PSSessionConfiguration -name Microsoft.powershell -showsecuritydescriptorUI # Add remote account
 get-netfirewallrule |where displayname -like "windows management instrumentation*" |set-netfirewallrule -enable True -verbose
 get-netfirewallrule |where displaygroup -EQ 'remote service management' |set-netfirewallrule -enable True -verbose
 
-
-
+#List Default Variables
 get-childitem env: |more
 $env:path
 get-variable |more
