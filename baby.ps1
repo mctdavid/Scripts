46a3daf87bc3d12a9f9ea2f75df4424b61966da3
mctdavid <36972430+mctdavid@users.noreply.github.com>
2019-09-30 19:49:15 -0700

    Create baby.ps1

---
+++ baby.ps1
@@ -0,0 +1,18 @@
+get-history | out-file c:\history.txt
+
+Start-transcript -path c:\transcript.txt -append
+stop-transcript
+
+get-service -computername Client01
+Enter-PSsession -computername Client01
+
+enable-psremoting -force
+Set-PSSessionConfiguration -name Microsoft.powershell -showsecuritydescriptorUI
+get-netfirewallrule |where displayname -like "windows management instrumentation*" |set-netfirewallrule -enable True -verbose
+get-netfirewallrule |where displaygroup -EQ 'remote service management' |set-netfirewallrule -enable True -verbose
+
+
+
+get-childitem env: |more
+$env:path
+get-variable |more
